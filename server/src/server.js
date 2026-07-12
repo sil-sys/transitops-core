8b3c93d8c65f1fdfd4532f7d0a15351881812988
sil-sys <silshah0801@gmail.com>
2026-07-12 12:27:53 +0530

    Finalize Atlas-ready server startup

--- server/src/server.js
+++ server/src/server.js
@@ -1,4 +1,8 @@
 require('dotenv').config();
+
+const dns = require('dns');
+dns.setServers(['8.8.8.8', '8.8.4.4']);
+
 const app = require('./app');
 const connectDB = require('./config/db');
 const User = require('./models/User');
